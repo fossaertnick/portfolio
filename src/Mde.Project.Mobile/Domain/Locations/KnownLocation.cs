0d29402266e1bdb1151ecbe1809c12b19b4363c5
FossaertN <nick.fossaert@student.howest.be>
2026-04-14 19:00:18 -0300

    create seeding data of a more complex form

---
+++ src/Mde.Project.Mobile/Domain/Locations/KnownLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Domain.Locations
+{
+    public class KnownLocation : Location
+    {
+        // properties
+        public string Name { get; set; }
+        public DateTime CreatedOn { get; set; } 
+        public string Occation { get; set; }
+    }
+}
---
+++ src/Mde.Project.Mobile/Domain/Locations/Mock/MockCurrentLocationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Domain.Locations.Mock
+{
+    class MockCurrentLocationService : ICurrentLocationService
+    {
+        public Location LastLocation { get; set; } = null;
+
+        public async Task StartTrackingAsync()
+        {
+            await Task.Delay(500);
+            LastLocation = new Location(0, 0);
+        }
+
+        public void StopTracking()
+        {
+            LastLocation = null;
+        }
+    }
+}
