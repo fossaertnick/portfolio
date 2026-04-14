b762fa580f3d9f5e285691fcf6b405f559ebb0fd
FossaertN <nick.fossaert@student.howest.be>
2026-04-14 19:01:40 -0300

    confir my pages and viewmodels in the project

--- src/Mde.Project.Mobile/MauiProgram.cs
+++ src/Mde.Project.Mobile/MauiProgram.cs
@@ -3,6 +3,9 @@ using Mde.Project.Mobile.Pages;
 using Microsoft.Extensions.Logging;
 using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Controls.Maps;
+using Mde.Project.Mobile.Domain.Locations;
+using Mde.Project.Mobile.Domain.Locations.Mock;
+using Mde.Project.Mobile.ViewModels;
 
 namespace Mde.Project.Mobile
 {
@@ -51,6 +54,26 @@ namespace Mde.Project.Mobile
             Routing.RegisterRoute("add", typeof(CreateOrUpdatePage));
             Routing.RegisterRoute("details", typeof(DetailsPage));
             Routing.RegisterRoute("map", typeof(MapPage));
+
+            builder.Services.AddSingleton<ListPage>();
+            builder.Services.AddTransient<ListViewModel>();
+
+            builder.Services.AddSingleton<DetailsPage>();
+            builder.Services.AddTransient<DetailsViewModel>();
+
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddTransient<MainViewModel>();
+
+            builder.Services.AddSingleton<MapPage>();
+            builder.Services.AddTransient<MapViewModel>();
+
+            builder.Services.AddSingleton<SettingsPage>();
+            builder.Services.AddTransient<SettingsViewModel>();
+
+            builder.Services.AddSingleton<CreateOrUpdatePage>();
+            builder.Services.AddTransient<CreateOrUpdateViewModel>();
+
+            builder.Services.AddTransient<IKnownLocationService, MockLocationService>();
 
             return builder.Build();
         }
