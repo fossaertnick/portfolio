79bc218f690f31a728b197ec2d776f7c33ef916f
FossaertN <nick.fossaert@student.howest.be>
2026-04-18 16:28:01 -0300

    Update services to current level of use

--- src/Mde.Project.Mobile/Domain/Locations/Memoria.cs
+++ src/Mde.Project.Mobile/Domain/Locations/Memoria.cs
@@ -4,11 +4,14 @@ using System.Text;
 
 namespace Mde.Project.Mobile.Domain.Locations
 {
-    public class KnownLocation : Location
+    public class Memoria : Location
     {
         // properties
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedOn { get; set; } 
+        public DateTime LastEditedOn { get; set; }
         public string Occation { get; set; }
+        public string Description { get; set; }
     }
 }
