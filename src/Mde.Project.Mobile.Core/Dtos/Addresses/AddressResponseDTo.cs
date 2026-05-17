284c018ea9da039b06049d84fd31a9c4932f1f8a
FossaertN <nick.fossaert@student.howest.be>
2026-05-17 16:26:55 -0300

    Introduce dto into core project

---
+++ src/Mde.Project.Mobile.Core/Dtos/Addresses/AddressResponseDTo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Core.Dtos.Addresses
+{
+    public class AddressResponseDTo
+    {
+        // properties
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string? HouseNumber { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ src/Mde.Project.Mobile.Core/Dtos/Memoria/MemoriaUpdateRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using Mde.Project.Mobile.Core.Dtos.Addresses;
+using Mde.Project.Mobile.Core.Dtos.MediaItem;
+using Mde.Project.Mobile.Core.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Core.Dtos.Memoria
+{
+    public class MemoriaUpdateRequestDto
+    {
+        // properties
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public OccationType Occation { get; set; }
+        public string? Description { get; set; }
+        public DateTime EventDate { get; set; }
+        public AddressRequestDto Address { get; set; }
+        public List<MediaItemRequestDto>? MediaMaterial { get; set; } = new();
+    }
+}
