37bd445b46b66638b4745a95eb6d0fbcfb7e2ea1
FossaertN <nick.fossaert@student.howest.be>
2026-05-20 14:10:59 -0300

    Introduce fonticons into app

--- src/Mde.Project.Mobile/MauiProgram.cs
+++ src/Mde.Project.Mobile/MauiProgram.cs
@@ -27,6 +27,7 @@ namespace Mde.Project.Mobile
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("icomoon.ttf", "appIcons");
                 });
 
             // SQLITE
