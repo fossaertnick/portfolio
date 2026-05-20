5067d4ffc03d0629017066f1dcb72e714ffd3021
FossaertN <nick.fossaert@student.howest.be>
2026-05-20 14:10:43 -0300

    Rewrite occationtypes to english

--- src/Mde.SourceOfTruth.Core/Migrations/20260519154148_InitialMigraiton.Designer.cs
+++ src/Mde.SourceOfTruth.Core/Migrations/20260519154148_InitialMigraiton.Designer.cs
@@ -4,6 +4,7 @@ using Mde.SourceOfTruth.Core.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mde.SourceOfTruth.Core.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260519154148_InitialMigraiton")]
+    partial class InitialMigraiton
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
